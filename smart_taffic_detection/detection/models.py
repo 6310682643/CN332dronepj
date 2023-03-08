40d0427673be5b101b46b314a7a35cf13d7f8ba9
6310682643 <6310682643@student.tu.ac.th>
2023-03-09 00:44:39 +0700

    first model

--- smart_taffic_detection/detection/models.py
+++ smart_taffic_detection/detection/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Input(models.Model):
+    time_record = models.TimeField('time', auto_now_add=True)
+    date_record = models.DateTimeField('date', auto_now_add=True)
+    video = models.FileField(upload_to='uploads/', blank=True)
+    location = models.CharField(max_length=100, default="")
+    def __str__(self):
+        return f"{self.video}"
+    
+class Result(models.Model):
+    video = models.FileField(upload_to='uploads/', blank=True)  
+
+    def __str__(self):
+        return f"{self.video}"
