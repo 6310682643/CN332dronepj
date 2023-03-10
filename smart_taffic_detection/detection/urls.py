de7624ae16997bc56391935daec7ea0d50ea116a
Suteekran Kangkreethaphol <suteekran07@gmail.com>
2023-03-11 00:37:55 +0700

    add field in models

--- smart_taffic_detection/detection/urls.py
+++ smart_taffic_detection/detection/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('upload', views.uploadPage, name="upload"),
+    path('upload/', views.uploadPage, name="upload"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- smart_taffic_detection/detection/views.py
+++ smart_taffic_detection/detection/views.py
@@ -1,18 +1,32 @@
 
 from django.shortcuts import render
 from .models import Input, Result
-
+from django.utils import timezone
 # Create your views here.
 
 def uploadPage(request):
+    print(timezone.now().strftime('%H:%M:%S.%f')[:-3])
     if request.method == "GET":
         return render(request, "upload.html")
     if request.method == "POST" and (request.FILES.get('video') is not None ):
-        print(request.FILES)
         video = request.FILES['video']
-        print(video)
         location = request.POST['location']
-        input = Input.objects.create(video=video, location=location)
+        time = request.POST.get('time') if request.POST.get('time') is not None else timezone.now()
+        date = request.POST.get('date') if request.POST.get('date') is not None else timezone.now()
+        print(request.POST.get('traffic_status'))
+        traffic_status = request.POST.get('traffic_status') if request.POST.get('traffic_status') is not None else 0
+        note = request.POST['note'] if request.POST.get('note') is not None else ""
+        weather = request.POST['weather'] if request.POST.get('weather') is not None else "Sunny"
+        input = Input.objects.create(
+            time_record=time, 
+            date_record=date, 
+            video=video, 
+            location=location, 
+            traffic_status=traffic_status, 
+            note=note,
+            weather=weather 
+        )
+   
         return render(request, "upload.html")
     else:
         return render(request, "upload.html")
