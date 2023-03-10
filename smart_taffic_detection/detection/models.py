ddf76e61b87883b205c9b6c86f1e10349d3c9bbf
Suteekran Kangkreethaphol <suteekran07@gmail.com>
2023-03-10 17:01:37 +0700

    setup upload function

--- smart_taffic_detection/detection/models.py
+++ smart_taffic_detection/detection/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Input(models.Model):
     time_record = models.TimeField('time', auto_now_add=True)
     date_record = models.DateTimeField('date', auto_now_add=True)
-    video = models.FileField(upload_to='uploads/', blank=True)
+    video = models.FileField(upload_to='uploads/video', blank=True)
     location = models.CharField(max_length=100, default="")
     def __str__(self):
         return f"{self.video}"
     
 class Result(models.Model):
-    video = models.FileField(upload_to='uploads/', blank=True)  
+    video = models.FileField(upload_to='uploads/video', blank=True)  
+    loop = models.FileField(upload_to='uploads/files', blank=True)
 
     def __str__(self):
         return f"{self.video}"
--- smart_taffic_detection/detection/views.py
+++ smart_taffic_detection/detection/views.py
@@ -1,3 +1,21 @@
+
 from django.shortcuts import render
+from .models import Input, Result
 
 # Create your views here.
+
+def uploadPage(request):
+    if request.method == "GET":
+        return render(request, "upload.html")
+    if request.method == "POST" and (request.FILES.get('video') is not None ):
+        print(request.FILES)
+        video = request.FILES['video']
+        print(video)
+        location = request.POST['location']
+        input = Input.objects.create(video=video, location=location)
+        return render(request, "upload.html")
+    else:
+        return render(request, "upload.html")
+
+
+    
