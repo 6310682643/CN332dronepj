c0250a18c3eb6547446440725b77f1aabbca330b
6310682643 <6310682643@student.tu.ac.th>
2023-03-11 20:02:35 +0700

    update

--- smart_taffic_detection/detection/urls.py
+++ smart_taffic_detection/detection/urls.py
@@ -4,7 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('upload/', views.uploadPage, name="upload"),
+    path('', views.loginPage, name="loginPage"),
+    path('upload/', views.uploadPage, name="uploadPage"),
+    # path('search/', views.searchBar, name='search'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('home/', views.home, name="home"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- smart_taffic_detection/detection/views.py
+++ smart_taffic_detection/detection/views.py
@@ -1,18 +1,23 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Input, Result
 from django.utils import timezone
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def uploadPage(request):
     print(timezone.now().strftime('%H:%M:%S.%f')[:-3])
+    d = timezone.now()
     if request.method == "GET":
         return render(request, "upload.html")
     if request.method == "POST" and (request.FILES.get('video') is not None ):
+        ownerName = request.POST['ownerName']
         video = request.FILES['video']
         location = request.POST['location']
-        time = request.POST.get('time') if request.POST.get('time') is not None else timezone.now()
-        date = request.POST.get('date') if request.POST.get('date') is not None else timezone.now()
+        time = request.POST.get('time') if request.POST.get('time') != "" else d.strftime("%H:%M:%S")
+        date = request.POST.get('date') if request.POST.get('date') != "" else d.strftime("%Y-%m-%d")
         print(request.POST.get('traffic_status'))
         traffic_status = request.POST.get('traffic_status') if request.POST.get('traffic_status') is not None else 0
         note = request.POST['note'] if request.POST.get('note') is not None else ""
@@ -24,12 +29,59 @@ def uploadPage(request):
             location=location, 
             traffic_status=traffic_status, 
             note=note,
-            weather=weather 
+            weather=weather,  
+            ownerName=ownerName
         )
    
-        return render(request, "upload.html")
+        return render(request, "home.html")
     else:
-        return render(request, "upload.html")
+        return render(request, "home.html")
 
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser:
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                return uploadPage(request)
+        else:
+            return render(request, 'login.html', {
+                'message': 'Invalid credentials.'
+            }, status=400)
 
+    return render(request, 'login.html')
+
+def homeStatus(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("loginPage"))
     
+def home(request):
+  task = Input.objects.all()
+#   template = loader.get_template('home.html')
+  return render(request, 'home.html', {'task': task,})
+
+def delete(request, id):
+    task = Input.objects.get(pk=id)
+    task.delete()
+    return redirect('home')
+
+
+
+# def searchBar(request):
+#     if not request.user.is_authenticated:
+#         return HttpResponseRedirect(reverse("login"))
+#     user_id = request.user.id
+
+#     if request.method == "GET":
+#         searched = request.GET.get('searched')
+#         if searched:
+#             blogs = Blog.objects.filter(title__contains=searched)
+#             return render(request, 'users/searchfor.html', {'blogs': blogs})
+#         else:
+#             print("No information to show")
+#             return render(request, 'users/searchfor.html', {'wallet':wallet})
