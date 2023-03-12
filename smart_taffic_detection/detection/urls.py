69d2e95a2c1eefe3ea299d9d92a1921bb86a33f3
6310682643 <6310682643@student.tu.ac.th>
2023-03-12 21:52:09 +0700

    edit func

--- smart_taffic_detection/detection/urls.py
+++ smart_taffic_detection/detection/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('upload/', views.uploadPage, name="uploadPage"),
     # path('search/', views.searchBar, name='search'),
     path('delete/<int:id>', views.delete, name='delete'),
+    path('edit/<int:id>', views.edit, name='edit'),
     path('home/', views.home, name="home"),
 ]
 
