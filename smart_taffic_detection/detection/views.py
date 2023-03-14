a11c2f0e369bb84bfbd02a5f5e9f874b275a7121
Parames Raikhao <6310680258@cn330>
2023-03-15 01:43:45 +0700

    add source video and suit to celery

--- smart_taffic_detection/detection/views.py
+++ smart_taffic_detection/detection/views.py
@@ -5,44 +5,61 @@ from django.utils import timezone
 from django.urls import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
+# from celery.result import AsyncResult
+# from django.http import JsonResponse
 # Create your views here.
+
 
 def uploadPage(request):
     print(timezone.now().strftime('%H:%M:%S.%f')[:-3])
     d = timezone.now()
     if request.method == "GET":
         return render(request, "upload.html")
-    if request.method == "POST" and (request.FILES.get('video') is not None ):
+    if request.method == "POST" and (request.FILES.get('video') is not None):
         ownerName = request.POST['ownerName']
         video = request.FILES['video']
         location = request.POST['location']
-        time = request.POST.get('time') if request.POST.get('time') != "" else d.strftime("%H:%M:%S")
-        date = request.POST.get('date') if request.POST.get('date') != "" else d.strftime("%Y-%m-%d")
+        time = request.POST.get('time') if request.POST.get(
+            'time') != "" else d.strftime("%H:%M:%S")
+        date = request.POST.get('date') if request.POST.get(
+            'date') != "" else d.strftime("%Y-%m-%d")
         print(request.POST.get('traffic_status'))
-        traffic_status = request.POST.get('traffic_status') if request.POST.get('traffic_status') is not None else 0
-        note = request.POST['note'] if request.POST.get('note') is not None else ""
-        weather = request.POST['weather'] if request.POST.get('weather') is not None else "Sunny"
+        traffic_status = request.POST.get('traffic_status') if request.POST.get(
+            'traffic_status') is not None else 0
+        note = request.POST['note'] if request.POST.get(
+            'note') is not None else ""
+        weather = request.POST['weather'] if request.POST.get(
+            'weather') is not None else "Sunny"
+        
         input = Input.objects.create(
-            time_record=time, 
-            date_record=date, 
-            video=video, 
-            location=location, 
-            traffic_status=traffic_status, 
+            time_record=time,
+            date_record=date,
+            video=video,
+            location=location,
+            traffic_status=traffic_status,
             note=note,
-            weather=weather,  
+            weather=weather,
             ownerName=ownerName
         )
-        call_detect.delay()
+
+        result = call_detect.delay('./' + input.video.url)
+
         return HttpResponseRedirect(reverse('home'))
     else:
         return render(request, "upload.html")
+
+# def celery_status(request, task_id):
+#     result = AsyncResult(task_id)
+#     response = {'status': result.status}
+#     return JsonResponse(response)
+
 
 def loginPage(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-        
+
         if user is not None:
             login(request, user)
             if request.user.is_superuser:
@@ -56,14 +73,17 @@ def loginPage(request):
 
     return render(request, 'login.html')
 
+
 def homeStatus(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("loginPage"))
-    
+
+
 def home(request):
-  task = Input.objects.all()
+    task = Input.objects.all()
 #   template = loader.get_template('home.html')
-  return render(request, 'home.html', {'task': task,})
+    return render(request, 'home.html', {'task': task, })
+
 
 def delete(request, id):
     task = Input.objects.get(pk=id)
